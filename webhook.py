373a99068857e5e3306d2b1676b1eab162e7f328
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-04-03 00:00:13 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -8,22 +8,22 @@ from telebot import types
 from main import bot
 
 
-# class handler(BaseHTTPRequestHandler):
-#     server_version = 'WebhookHandler/1.0'
+class handler(BaseHTTPRequestHandler):
+    server_version = 'WebhookHandler/1.0'
 
-#     def do_GET(self):
-#         time.sleep(1.5)
-#         bot.set_webhook('https://' + 'bbbb-alpha.vercel.app/')
-#         self.send_response(200)
-#         self.end_headers()
+    def do_GET(self):
+        time.sleep(1.5)
+        bot.set_webhook('https://' + 'bbbb-alpha.vercel.app/')
+        self.send_response(200)
+        self.end_headers()
 
-#     def do_POST(self):
-#         cl = int(self.headers['Content-Length'])
-#         post_data = self.rfile.read(cl)
-#         body = json.loads(post_data.decode())
+    def do_POST(self):
+        cl = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(cl)
+        body = json.loads(post_data.decode())
 
-#         bot.process_new_updates([types.Update.de_json(body)])
+        bot.process_new_updates([types.Update.de_json(body)])
 
-#         self.send_response(204)
-#         self.end_headers()
-bot.pooling()
+        self.send_response(204)
+        self.end_headers()
+
