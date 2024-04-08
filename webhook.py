48374c97a4c3d8cb165dae8e871f90df0d19b69d
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-04-09 00:24:45 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -11,7 +11,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        with open(join('data', 'file.txt'), 'r') as file:
+        with open(join('..', 'file.txt'), 'r') as file:
           for line in file:
             self.wfile.write(line.encode())
         return
