76d0e8349cb92c3a4ad767d9b84b9f54f39ff24e
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-04-09 00:40:43 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -4,13 +4,16 @@ import time
 from http.server import BaseHTTPRequestHandler
 from telebot import types
 from main import bot
+
 class handler(BaseHTTPRequestHandler):
     server_version = 'WebhookHandler/1.0'
-     def do_GET(self):
+
+    def do_GET(self):
         time.sleep(2)
         bot.set_webhook('https://' + 'bbbb-alpha.vercel.app')
         self.send_response(200)
         self.end_headers()
+
     def do_POST(self):
         cl = int(self.headers['Content-Length'])
         post_data = self.rfile.read(cl)
