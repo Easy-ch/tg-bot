acac2cf2f6be750c2555abd61f708d0bda06c45e
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-04-13 00:27:09 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from bot import bot
 from telegram import Update,Bot
 
-URL = "bbbb-alpha.vercel.app"
+URL = "vercel.com/bbbb/easys-projects/"
 TOKEN = "6430079230:AAGxyL2dzCo2LJFSwuTxtmguVKv2fdlxLYw"
 app = Flask(__name__)
 
