7af29844cd9ee0174ccd4e267931f75f7273cd60
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-04-12 23:58:53 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
-from .bot import bot
-URL = "vercel.com/easys-projects/"
+from bot import bot
+from telegram import Update,Bot
+bot.delete_webhook()
+URL = "https://vercel.com/easys-projects/"
 TOKEN = "6430079230:AAGxyL2dzCo2LJFSwuTxtmguVKv2fdlxLYw"
 app = Flask(__name__)
 
@@ -8,17 +10,16 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-
-@app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
+@app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
     update = Update.de_json(request.get_json(force=True), bot)
-    setup().process_update(update)
+    bot.process_update(update)
     return 'ok'
-
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    bot_instance = Bot(TOKEN)
+    s = bot_instance.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
     if s:
         return "webhook setup ok"
     else:
