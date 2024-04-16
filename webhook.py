b9dd98dfa26e83de5beb421c5d5e57af12c4bc35
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-04-16 20:16:13 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,25 +1,8 @@
 import json
 from http.server import SimpleHTTPRequestHandler
-import requests
 import time 
 from telebot import types
 from bot import bot  # Подключаем вашего бота из другого файла
-server_url = "https://bbbb-alpha.vercel.app"
-update_interval = 120
-def update_server():
-    try:
-        response = requests.get(server_url)
-        if response.status_code == 200:
-            print("Страница успешно обновлена.")
-        else:
-            print(f"Ошибка при обновлении страницы. Код состояния: {response.status_code}")
-    except Exception as e:
-        print(f"Ошибка при попытке обновления страницы: {str(e)}")
-
-if __name__ == "__main__":
-    while True:
-        update_server()
-        time.sleep(update_interval)
 class handler(SimpleHTTPRequestHandler):
     server_version = 'WebhookHandler/1.0'
 
