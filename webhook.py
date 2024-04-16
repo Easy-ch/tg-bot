d2fb0cba49e6a6dc980a740f21c46646e9f0773c
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-04-16 20:13:19 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,9 +1,25 @@
 import json
 from http.server import SimpleHTTPRequestHandler
-
+import requests
+import time 
 from telebot import types
 from bot import bot  # Подключаем вашего бота из другого файла
+server_url = "https://bbbb-alpha.vercel.app"
+update_interval = 120
+def update_server():
+    try:
+        response = requests.get(server_url)
+        if response.status_code == 200:
+            print("Страница успешно обновлена.")
+        else:
+            print(f"Ошибка при обновлении страницы. Код состояния: {response.status_code}")
+    except Exception as e:
+        print(f"Ошибка при попытке обновления страницы: {str(e)}")
 
+if __name__ == "__main__":
+    while True:
+        update_server()
+        time.sleep(update_interval)
 class handler(SimpleHTTPRequestHandler):
     server_version = 'WebhookHandler/1.0'
 
@@ -27,6 +43,7 @@ class handler(SimpleHTTPRequestHandler):
             self.end_headers()
         except Exception as e:
             print(e)
+my_handler = handler()
 
 # Функция, которая будет вызываться Vercel при обработке запросов
 def vercel_handler(request):
@@ -40,5 +57,5 @@ def vercel_handler(request):
 if __name__ == '__main__':
     from http.server import HTTPServer
     import os
-    server = HTTPServer(('0.0.0.0', int(os.environ.get('PORT', 5000))))
+    server = HTTPServer(('0.0.0.0', int(os.environ.get('PORT', 5000))), my_handler)
     server.serve_forever()
