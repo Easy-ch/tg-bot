7f3707a5f4191553b4103c66f3d5b48e7f49f617
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-05-26 19:03:24 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,39 +1,68 @@
-from fastapi import FastAPI, Request
-from aiogram import types, Dispatcher, Bot
+from flask import Flask, request
+from aiogram import types,Bot,Dispatcher
 from bot import dp, bot,register_handlers
 from dotenv import load_dotenv
 import os
+import requests
+import asyncio
 load_dotenv()
 register_handlers(dp)
-app = FastAPI()
+app = Flask(__name__)
 TOKEN = os.getenv('TOKEN')
-WEBHOOK_HOST = 'https://bbbb-alpha.vercel.app/'
+WEBHOOK_HOST = 'https://34e0-188-243-182-2.ngrok-free.app'
 WEBHOOK_PATH = '/webhook'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-@app.on_event("startup")
+# @app.on_event("startup")
+# async def on_startup():
+#     webhook_info = await bot.get_webhook_info()
+#     if webhook_info.url != WEBHOOK_URL:
+
+
+# @app.post(WEBHOOK_PATH)
+# async def bot_webhook(request: Request):
+#     telegram_update = types.Update(await request.json())
+#     Dispatcher.set_current(dp)
+#     Bot.set_current(bot)
+#     await dp.process_update(telegram_update)
+@app.route(WEBHOOK_PATH, methods=['POST'])
+async def webhook():
+    update = request.get_json()
+    if not update:
+        return 'ПУСТОЙ!!!!!!!!!!!!', 404
+    # await dp.process_update(types.Update(**update))
+    Dispatcher.set_current(dp)
+    Bot.set_current(bot)
+    await dp.process_update(types.Update(**update))
+    return 'OK', 200
+# @app.on_event("shutdown")
+# async def on_shutdown():
+#     await bot.session.close(
 async def on_startup():
     webhook_info = await bot.get_webhook_info()
     if webhook_info.url != WEBHOOK_URL:
-        await bot.set_webhook(
-            url=WEBHOOK_URL
-        )
+        await bot.set_webhook(url=WEBHOOK_URL)
+        print("Webhook set successfully!")
+    else:
+        print("Webhook was already set")
 
-
-@app.post(WEBHOOK_PATH)
-async def bot_webhook(request: Request):
-    telegram_update = types.Update(await request.json())
-    Dispatcher.set_current(dp)
-    Bot.set_current(bot)
-    await dp.process_update(telegram_update)
-
-
-@app.on_event("shutdown")
 async def on_shutdown():
     await bot.session.close()
+
+
+
 @app.get("/")
 async def root():
     return {"message": "Bot webhook is running"}
-if __name__ == "__main__":
-    from aiogram import executor
-    executor.start_polling(dp)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(on_startup())
+    response = requests.get(f'https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}')
+    if response.status_code == 200:
+        print("Webhook set successfully!")
+    else:
+        print("Failed to set webhook")
+    app.run(host='0.0.0.0',port = 80,debug=True)
+
+
