7252e6afc91e63301ce4c8057462fee676c4a170
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-05-29 23:54:20 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,46 +1,35 @@
-import logging
-import os
-from aiohttp import web
+from fastapi import FastAPI
+from aiogram import types, Dispatcher, Bot
+from bot import dp, bot,register_handlers
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from bot import register_handlers
-
+import os
 load_dotenv()
+register_handlers(dp)
+app = FastAPI()
+TOKEN = str(os.getenv('TOKEN'))
+WEBHOOK_PATH = f"/bot/{TOKEN}"
+WEBHOOK_URL = f"{'https://bbbb-alpha.vercel.app'}{WEBHOOK_PATH}"
 
-TOKEN = os.getenv('TOKEN')
-WEBHOOK_HOST = 'https://bbbb-alpha.vercel.app/'
-WEBHOOK_PATH = '/webhook'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-logging.basicConfig(level=logging.INFO)
+@app.on_event("startup")
+async def on_startup():
+    webhook_info = await bot.get_webhook_info()
+    if webhook_info.url != WEBHOOK_URL:
+        await bot.set_webhook(
+            url=WEBHOOK_URL
+        )
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
 
-register_handlers(dp)  # Регистрация обработчиков
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(update: dict):
+    telegram_update = types.Update(**update)
+    Dispatcher.set_current(dp)
+    Bot.set_current(bot)
+    await dp.process_update(telegram_update)
 
-async def on_startup(app):
-    logging.info('Starting up...')
-    await bot.set_webhook(WEBHOOK_URL)
-    logging.info(f"Webhook URL: {WEBHOOK_URL}")
 
-async def on_shutdown(app):
-    logging.warning('Shutting down..')
-    await bot.delete_webhook()
-    await dp.storage.close()
-    logging.warning('Bye!')
+@app.on_event("shutdown")
+async def on_shutdown():
+    await bot.session.close()
 
-app = web.Application()
-SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
-setup_application(app, dp, bot=bot)
-
-app.on_startup.append(on_startup)
-app.on_shutdown.append(on_shutdown)
-
-def handler(request, *args):
-    return app._handle(request)
-
-if __name__ == '__main__':
-    web.run_app(app)
+app = app
