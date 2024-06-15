7c844f49a7546183a18c1cd844b79ed5a61c90f8
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-06-16 01:30:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,11 +18,8 @@ course : float
 storage = MemoryStorage()
 dp = Dispatcher(bot,storage=storage)
 dp.middleware.setup(LoggingMiddleware())
-async def deleteweb(bot:Bot):
-    await bot.delete_webhook()
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
-    await deleteweb(bot)
     await dp.start_polling(bot)
 
 @dp.message_handler(CommandStart())
