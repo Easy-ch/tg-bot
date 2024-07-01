5d9a1a30adddd29cac711477bcf24b68834e7638
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-07-01 16:21:03 +0300

    Global Update

--- db.py
+++ db.py
@@ -1,24 +1,106 @@
 import asyncpg
-from config import DATABASE_URL
 import asyncio
-async def create_table():
-    conn = await asyncpg.connect(DATABASE_URL)
-    await conn.execute('''
-        CREATE TABLE IF NOT EXISTS course (
+# import asyncio
+class Database:
+    _pool = None
+    def __init__(self,host='127.0.0.1',user='easych',password='admin',database='bot_end') -> None:
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+    @classmethod
+    async def connect(cls,**kwargs):
+        if cls._pool is None:
+            cls._pool = await asyncpg.create_pool(**kwargs)
+        return cls._pool
+    @classmethod
+    async def close(cls):
+        if cls._pool is not None:
+            await asyncpg.close()
+            cls._pool = None
+    @classmethod
+    async def fetchrow(cls,query,*args):
+        async with cls._pool.acquire() as connection:
+            return await connection.fetchrow(query,*args)
+    @classmethod
+    async def execute(cls,query,*args):
+        async with cls._pool.acquire() as connection:
+            return await connection.execute(query,*args)
+    @classmethod
+    async def fetch(cls,query,*args):
+        async with cls._pool.acquire() as connection:
+            return await connection.fetch(query,*args)
+
+class Course:
+    @classmethod 
+    async def create_table(cls):
+        query = """
+    CREATE TABLE IF NOT EXISTS course (
             id SERIAL PRIMARY KEY,
-            value REAL NOT NULL
-        )
-    ''')
-    await conn.close()
+            value REAL NOT NULL)
+    """
+        return await Database.execute(query)
+    @classmethod
+    async def set_course(cls,value):
+        query = 'INSERT INTO course (value) VALUES ($1)'
+        return  await Database.execute(query,value)
+    @classmethod
+    async def get_course(cls):
+        query = 'SELECT value FROM course ORDER BY id DESC LIMIT 1'
+        row = await Database.fetchrow(query)
+        return row['value'] if row else None
+    
+class Order:
+    @classmethod
+    async def create_table(cls):
+        query = """
+    CREATE TABLE IF NOT EXISTS orders (
+        id SERIAL PRIMARY KEY, 
+        order_id  VARCHAR(50) NOT NULL,
+        description TEXT,
+        status VARCHAR(50) NOT NULL,  
+        access_password VARCHAR(255) NOT NULL  
+    );
+    """
+        return await Database.execute(query)
+    @classmethod
+    async def add_order(cls,order_id,description,status,access_password):
+        query ='INSERT INTO orders (order_id,description,status,access_password) VALUES ($1,$2,$3,$4)'
+        return await Database.execute(query,order_id,description,status,access_password)
 
-async def set_course(value):
-    conn = await asyncpg.connect(DATABASE_URL)
-    await conn.execute('INSERT INTO course (value) VALUES ($1)', value)
-    await conn.close()
+    @classmethod
+    async def password_valid(cls, order_id, access_password):
+        query = 'SELECT 1 FROM orders WHERE order_id = $1 AND access_password = $2'
+        row = await Database.fetchrow(query, order_id, access_password)
+        return row is not None
+    
+    @classmethod
+    async def order_exists(cls, order_id):
+        query = 'SELECT 1 FROM orders WHERE order_id = $1'
+        row = await Database.fetchrow(query, order_id)
+        return row is not None
 
-async def get_course():
-    conn = await asyncpg.connect(DATABASE_URL)
-    row = await conn.fetchrow('SELECT value FROM course ORDER BY id DESC LIMIT 1')
-    await conn.close()
-    return row['value'] if row else None
-asyncio.get_event_loop().run_until_complete(create_table())
+
+    @classmethod
+    async def get_order(cls,order_id):
+        query = 'SELECT * FROM orders WHERE order_id = $1'
+        row = await Database.fetchrow(query,order_id)
+        return row
+    
+    @classmethod 
+    async def remove_order(cls,order_id):
+        query = """
+        DELETE FROM orders
+        WHERE order_id = $1;
+        """
+        return await Database.execute(query,order_id)
+    
+    @classmethod
+    async def change_status_order(cls,order_id,status):
+        query = """
+        UPDATE orders
+        SET status = $2
+        WHERE order_id = $1
+        """
+        return await Database.execute(query,order_id,status)
+    
--- index.py
+++ index.py
@@ -1,23 +1,24 @@
 import logging
 from fastapi import FastAPI, Request
 from aiogram import types, Dispatcher, Bot
-from bot import dp, bot, register_handlers
+from handlers import dp, bot
+
 from config import TOKEN
+from db import Course,Database,Order
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Загрузка переменных окружения из .env файла
-register_handlers(dp)
+
 
 app = FastAPI()
 
 WEBHOOK_PATH = f"/{TOKEN}"
-WEBHOOK_URL = f"https://bbbb-alpha.vercel.app{WEBHOOK_PATH}"
+WEBHOOK_URL = f"https://dfe2-188-243-182-2.ngrok-free.app{WEBHOOK_PATH}"
 
 @app.on_event("startup")
 async def on_startup():
-    await bot.delete_webhook()
     webhook_info = await bot.get_webhook_info()
     await bot.set_webhook(url=WEBHOOK_URL)
     logger.info(f"Webhook URL set to: {WEBHOOK_URL}")
@@ -44,9 +45,9 @@ async def bot_webhook(request: Request):
 
 @app.on_event("shutdown")
 async def on_shutdown():
+    await Database.close()
     await bot.session.close()   
 
 # Экспорт приложения для Vercel
 
-    app=app
 
