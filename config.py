3db1b34349cf63768f9cbd6aed95e8e0a649732d
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-07-01 20:57:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 TOKEN = '6430079230:AAGxyL2dzCo2LJFSwuTxtmguVKv2fdlxLYw'
 ADMIN_ID = '5034422722'
-# DATABASE_URL = "postgres://default:cNAgyo0j6JGl@ep-soft-snowflake-a4b3n145.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+DATABASE_URL = "postgres://default:cNAgyo0j6JGl@ep-soft-snowflake-a4b3n145.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+POSTGRES_HOST = 'ep-soft-snowflake-a4b3n145-pooler.us-east-1.aws.neon.tech'
+POSTGRES_PASSWORD = 'cNAgyo0j6JGl'
+POSTGRES_USER ='default'
+POSTGRES_DATABASE = 'verceldb'
