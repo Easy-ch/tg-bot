fd0cfd50cb60079cf0bc20762776fbb50bb354d7
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-07-05 18:31:54 +0300

    Update index.py

--- index.py
+++ index.py
@@ -19,14 +19,19 @@ WEBHOOK_URL = f"https://7c05-188-243-182-2.ngrok-free.app{WEBHOOK_PATH}"
 
 @app.on_event("startup")
 async def on_startup():
-    await Database.connect(    
-        user='default', 
-        password='cNAgyo0j6JGl', 
-        database='verceldb', 
-        host='ep-soft-snowflake-a4b3n145-pooler.us-east-1.aws.neon.tech')
-    webhook_info = await bot.get_webhook_info()
-    await bot.set_webhook(url=WEBHOOK_URL)
-    logger.info(f"Webhook URL set to: {WEBHOOK_URL}")
+    logger.info("Starting up application")
+    try:
+        await Database.connect(
+            user=os.getenv('POSTGRES_USER'),
+            password=os.getenv('POSTGRES_PASSWORD'),
+            database=os.getenv('POSTGRES_DATABASE'),
+            host=os.getenv('POSTGRES_HOST')
+        )
+        webhook_info = await bot.get_webhook_info()
+        await bot.set_webhook(url=WEBHOOK_URL)
+        logger.info(f"Webhook URL set to: {WEBHOOK_URL}")
+    except Exception as e:
+        logger.error(f"Failed to connect to database or set webhook: {e}")
 
 @app.get("/")
 async def read_root():
