bad4eee84f600b7c065869292e8d4681508462a6
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-07-08 14:53:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 TOKEN = '6430079230:AAGxyL2dzCo2LJFSwuTxtmguVKv2fdlxLYw'
 ADMIN_ID = '5034422722'
-POSTGRES_HOST = 'ep-soft-snowflake-a4b3n145-pooler.us-east-1.aws.neon.tech'
-POSTGRES_PASSWORD = 'cNAgyo0j6JGl'
-POSTGRES_USER ='default'
-POSTGRES_DATABASE = 'verceldb'
-POSTGRES_URL="postgres://default:cNAgyo0j6JGl@ep-soft-snowflake-a4b3n145-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+TOKEN = '6430079230:AAGxyL2dzCo2LJFSwuTxtmguVKv2fdlxLYw'
+ADMIN_ID = '5034422722'
+POSTGRES_URL="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+POSTGRES_PRISMA_URL="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
+POSTGRES_URL_NO_SSL="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb"
+POSTGRES_URL_NON_POOLING="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+POSTGRES_USER="default"
+POSTGRES_HOST="ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech"
+POSTGRES_PASSWORD="8LmQsXOP0Jgi"
+POSTGRES_DATABASE="verceldb"
