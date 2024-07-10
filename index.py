c28b092e4c9710e8ccf43028989a48af8589b9ac
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-07-10 18:40:05 +0300

    Update index.py

--- index.py
+++ index.py
@@ -2,38 +2,38 @@ import logging
 from fastapi import FastAPI, Request
 from aiogram import types, Dispatcher, Bot
 from handlers import dp, bot
-from config import TOKEN,POSTGRES_DATABASE,POSTGRES_HOST,POSTGRES_PASSWORD,POSTGRES_USER
-from db import Database,Course,Order
+from dotenv import load_dotenv
+from config import TOKEN, POSTGRES_DATABASE, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_USER
+from db import Database, Course, Order
 import asyncio
+
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Загрузка переменных окружения из .env файла
+load_dotenv()
 
 app = FastAPI()
 
-
 WEBHOOK_PATH = f"/{TOKEN}"
-WEBHOOK_URL = f"https://bbbb-alpha.vercel.app/{TOKEN}"
-
-@app.on_event("startup")
-def on_startup():
-    asyncio.run(init())
+WEBHOOK_URL = f"https://eb2a-188-243-182-2.ngrok-free.app{WEBHOOK_PATH}"
 
 async def init():
     logger.info("Starting up application")
     try:
-        await Database.connect(user=POSTGRES_USER,password=POSTGRES_PASSWORD,database=POSTGRES_DATABASE,host=POSTGRES_HOST)  
+        await Database.connect(user=POSTGRES_USER, password=POSTGRES_PASSWORD, database=POSTGRES_DATABASE, host=POSTGRES_HOST)
         await Course.create_table()
         await Order.create_table()
-        
+        await bot.set_webhook(url=WEBHOOK_URL)
+        logger.info(f"Webhook URL set to: {WEBHOOK_URL}")
     except Exception as e:
-        logger.error(f"Failed to connect to database:{e}")
-    await bot.set_webhook(url=WEBHOOK_URL)
-    logger.info(f"Webhook URL set to: {WEBHOOK_URL}")
-    
-    
+        logger.error(f"Failed to initialize: {e}")
+
+@app.on_event("startup")
+def on_startup():
+    asyncio.create_task(init())
+
 @app.get("/")
 async def read_root():
     return {"message": "Webhook is set and running"}
@@ -53,12 +53,11 @@ async def bot_webhook(request: Request):
     except Exception as e:
         logger.error(f"Failed to process update: {e}")
         return {"status": "error", "message": str(e)}
-    
 
 @app.on_event("shutdown")
 async def on_shutdown():
     await Database.close()
-    await bot.session.close()   
+    await bot.session.close()
 
 # Экспорт приложения для Vercel
-app=app
+app = app
