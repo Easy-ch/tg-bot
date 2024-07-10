02e0b90bad35c203faba2e7a8b227ff4e5de46a5
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-07-10 17:32:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 TOKEN = '6430079230:AAGxyL2dzCo2LJFSwuTxtmguVKv2fdlxLYw'
 ADMIN_ID = '5034422722'
-TOKEN = '6430079230:AAGxyL2dzCo2LJFSwuTxtmguVKv2fdlxLYw'
-ADMIN_ID = '5034422722'
-POSTGRES_URL="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-POSTGRES_PRISMA_URL="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
-POSTGRES_URL_NO_SSL="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb"
-POSTGRES_URL_NON_POOLING="postgres://default:8LmQsXOP0Jgi@ep-late-lab-a4e96vwv.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+POSTGRES_URL="postgres://default:nNKJaXEgM1A9@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+POSTGRES_PRISMA_URL="postgres://default:nNKJaXEgM1A9@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
+POSTGRES_URL_NO_SSL="postgres://default:nNKJaXEgM1A9@ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech:5432/verceldb"
+POSTGRES_URL_NON_POOLING="postgres://default:nNKJaXEgM1A9@ep-late-lab-a4e96vwv.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
 POSTGRES_USER="default"
 POSTGRES_HOST="ep-late-lab-a4e96vwv-pooler.us-east-1.aws.neon.tech"
-POSTGRES_PASSWORD="8LmQsXOP0Jgi"
+POSTGRES_PASSWORD="nNKJaXEgM1A9"
 POSTGRES_DATABASE="verceldb"
